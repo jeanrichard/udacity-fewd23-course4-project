48711a55d37b84f7eb6e1ad682ebb703480b784c
Etienne Richard <etienne.jeanrichard@gmail.com>
2024-03-14 00:39:07 +0000

    test: Test fetch abort with fake timers

--- __test__/post-data-timed.test.js
+++ __test__/post-data-timed.test.js
@@ -1,39 +1,45 @@
 // @ts-check
 'use strict';
 
-import { enableFetchMocks } from 'jest-fetch-mock'
+import { enableFetchMocks } from 'jest-fetch-mock';
 enableFetchMocks();
 
 import { afterEach, beforeEach, describe, expect, it, jest } from '@jest/globals';
 import { postData } from '../src/client/js/form-handler.js';
 
-/*
-References:
-- https://jestjs.io/docs/asynchronous
-- https://jestjs.io/docs/timer-mocks
-- https://www.npmjs.com/package/jest-fetch-mock
-- https://testing-library.com/docs/using-fake-timers/
-*/
-
 describe('Testing timed functionality to POST a request and return a pair (response, JSON)', () => {
-
   beforeEach(() => {
+    // Fetch.
     /** @ts-ignore: Property 'resetMocks' does not exist on type ... */
     fetch.resetMocks();
     /** @ts-ignore: Property 'doMock' does not exist on type ... */
     fetch.doMock();
+    // Timers.
     jest.useFakeTimers();
   });
 
   afterEach(() => {
+    // Timers.
     jest.runOnlyPendingTimers();
     jest.useRealTimers();
+    // Mocks.
+    jest.clearAllMocks();
   });
 
-  it("throws a DOMException with name 'AbortError' if the timeout is exceeded", async () => {
+  it("throws a DOMException with name 'AbortError' if 'fetch' is aborted", async () => {
     /** @ts-ignore: Property 'mockAbortOnce' does not exist on type ... */
     fetch.mockAbortOnce();
     await expect(postData('https://example.com/')).rejects.toThrow('The operation was aborted.');
   });
 
+  it("throws a DOMException with name 'AbortError' if the timeout is exceeded", async () => {
+    /** @ts-ignore: Property 'mockResponseOnce' does not exist on type ... */
+    fetch.mockResponseOnce(async () => {
+      jest.advanceTimersByTime(20);
+      return '';
+    });
+    await expect(postData('https://example.com/', /*data=*/ {}, /*timeoutMs=*/ 10)).rejects.toThrow(
+      'The operation was aborted.',
+    );
+  });
 });
